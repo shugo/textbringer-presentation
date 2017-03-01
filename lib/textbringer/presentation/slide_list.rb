36057eb20f08fbaafaef6b8140bb0de96f3f73f1
Shugo Maeda <shugo@ruby-lang.org>
2017-03-01 22:02:06 +0900

    Add Presentation::SlideList.

---
+++ lib/textbringer/presentation/slide_list.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+module Textbringer
+  module Presentation
+    class Slide
+      attr_reader :number, :title, :body
+
+      def initialize(number, title, body)
+        @number = number
+        @title = title
+        @body = body
+      end
+    end
+
+    class SlideList
+      def initialize(s)
+        @list = s.scan(/^# *(.*?)\n(.*?)(?:(?=^#)|\z)/m).map.with_index {
+          |(title, body), i|
+          Slide.new(i + 1, title.strip, body.strip)
+        }
+        @index = 0
+      end
+
+      def size
+        @list.size
+      end
+
+      def current_index
+        @index
+      end
+
+      def current_slide
+        @list[@index]
+      end
+
+      def forward_slide
+        if @index < @list.size - 1
+          @index += 1
+        end
+      end
+
+      def backward_slide
+        if @index > 0
+          @index -= 1
+        end
+      end
+    end
+  end
+end
